0bbcde08d7ad05625f02bb4fba03537e68721a5d
Samuel Shih <samuelshih@Samuels-MacBook-Pro.local>
2020-12-18 08:15:24 +0800

    Added css positioning and more styling

---
+++ js/landing-page.js
@@ -0,0 +1,9 @@
+const input = document.querySelector('.submit-btn')
+const submitForm = (e) => {
+    e.preventDefault()
+    console.log('Form Submited')
+
+    // Fetch Data
+}
+
+input.addEventListener('click', submitForm)
